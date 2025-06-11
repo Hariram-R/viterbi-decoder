e130f31b649f59e183e27e7e52bd20f0ba08d259
arjitv99 <arjitv99@gmail.com>
2025-06-11 00:06:38 -0700

    fixes to tbu.sv

--- rtl/tbu.sv
+++ rtl/tbu.sv
@@ -3,9 +3,9 @@ module tbu
 	input       clk,
 	input       rst,
 	input       enable,
-	input       selection,	//FIXME: who's providing this input?
-	input [7:0] d_in_0,		//FIXME: who's providing this input?
-	input [7:0] d_in_1,		//FIXME: who's providing this input?
+	input       selection,	//set/unset by ping pong mechanism in decoder
+	input [7:0] d_in_0,		//Comes from rotating memory banks
+	input [7:0] d_in_1,		//Comes from rotating memory banks
 	output logic  d_o,
 	output logic  wr_en);
 	
@@ -30,72 +30,56 @@ See assignment text for details
 		if (!selection) begin
 			case(pstate)
 				3'b000: begin
-					//hemlo deer
-					nstate = d_in_0[pstate] ? 1 : 0;
+					nstate = d_in_0[pstate] ? 3'd1 : 3'd0;
 				end
 				3'b001: begin
-					//hemlo deer
-					nstate = d_in_0[pstate] ? 2 : 3;
+					nstate = d_in_0[pstate] ? 3'd2 : 3'd3;
 				end
 				3'b010: begin
-					//hemlo deer
-					nstate = d_in_0[pstate] ? 5 : 4;
+					nstate = d_in_0[pstate] ? 3'd5 : 3'd4;
 				end
 				3'b011: begin
-					//hemlo deer
-					nstate = d_in_0[pstate] ? 6 : 7;
+					nstate = d_in_0[pstate] ? 3'd6 : 3'd7;
 				end
 				3'b100: begin
-					//hemlo deer
-					nstate = d_in_0[pstate] ? 0 : 1;
+					nstate = d_in_0[pstate] ? 3'd0 : 3'd1;
 				end
 				3'b101: begin
-					//hemlo deer
-					nstate = d_in_0[pstate] ? 3 : 2;
+					nstate = d_in_0[pstate] ? 3'd3 : 3'd2;
 				end
 				3'b110: begin
-					//hemlo deer
-					nstate = d_in_0[pstate] ? 4 : 5;
+					nstate = d_in_0[pstate] ? 3'd4 : 3'd5;
 				end
 				3'b111: begin
-					//hemlo deer
-					nstate = d_in_0[pstate] ? 7 : 6;
+					nstate = d_in_0[pstate] ? 3'd7 : 3'd6;
 				end
 			endcase
 		end
 		else begin
 			case(pstate)
 				3'b000: begin
-					//hemlo deer
-					nstate = d_in_1[pstate] ? 1 : 0;
+					nstate = d_in_1[pstate] ? 3'd1 : 3'd0;
 				end
 				3'b001: begin
-					//hemlo deer
-					nstate = d_in_1[pstate] ? 2 : 3;
+					nstate = d_in_1[pstate] ? 3'd2 : 3'd3;
 				end
 				3'b010: begin
-					//hemlo deer
-					nstate = d_in_1[pstate] ? 5 : 4;
+					nstate = d_in_1[pstate] ? 3'd5 : 3'd4;
 				end
 				3'b011: begin
-					//hemlo deer
-					nstate = d_in_1[pstate] ? 6 : 7;
+					nstate = d_in_1[pstate] ? 3'd6 : 3'd7;
 				end
 				3'b100: begin
-					//hemlo deer
-					nstate = d_in_1[pstate] ? 0 : 1;
+					nstate = d_in_1[pstate] ? 3'd0 : 3'd1;
 				end
 				3'b101: begin
-					//hemlo deer
-					nstate = d_in_1[pstate] ? 3 : 2;
+					nstate = d_in_1[pstate] ? 3'd3 : 3'd2;
 				end
 				3'b110: begin
-					//hemlo deer
-					nstate = d_in_1[pstate] ? 4 : 5;
+					nstate = d_in_1[pstate] ? 3'd4 : 3'd5;
 				end
 				3'b111: begin
-					//hemlo deer
-					nstate = d_in_1[pstate] ? 7 : 6;
+					nstate = d_in_1[pstate] ? 3'd7 : 3'd6;
 				end
 			endcase
 		end
@@ -111,10 +95,10 @@ See assignment text for details
 	always @(posedge clk, negedge rst) begin
     //reset p state to 0
     //exception:  rst = enable = selection_buf = 1, but selection = 0, then go to n_state
-		if (!rst) pstate <= 0;
+		if (!rst) pstate <= 3'b000;
 		else begin
 			if (enable && selection_buf && !selection) pstate <= nstate;
-			else pstate <= pstate;
+			else pstate <= 3'b000;
 		end
 	end
 
